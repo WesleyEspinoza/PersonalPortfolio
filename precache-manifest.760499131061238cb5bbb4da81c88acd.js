b3d08f437c68abae36408cb245f830c7e6cbb71c
Erick Wesley Espinoza <Wesleyespinoza1021@gmail.com>
2019-09-26 13:59:41 -0700

    Updates

---
+++ precache-manifest.760499131061238cb5bbb4da81c88acd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1013eec1173105c09ab3047a40cde2f",
+    "url": "/PersonalPortfolio/index.html"
+  },
+  {
+    "revision": "fd1545aa481911813b07",
+    "url": "/PersonalPortfolio/static/css/main.e3899765.chunk.css"
+  },
+  {
+    "revision": "7fd791b2282426c68fa1",
+    "url": "/PersonalPortfolio/static/js/2.9afaadc4.chunk.js"
+  },
+  {
+    "revision": "fd1545aa481911813b07",
+    "url": "/PersonalPortfolio/static/js/main.44abef59.chunk.js"
+  },
+  {
+    "revision": "b4e4b850fcec9de91037",
+    "url": "/PersonalPortfolio/static/js/runtime~main.84d5d019.js"
+  }
+]);
